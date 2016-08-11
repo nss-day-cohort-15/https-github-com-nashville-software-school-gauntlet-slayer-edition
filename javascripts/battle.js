999a2357fc200dfc9b83076ea3ac49ac2e2c4fae
Jacob Keen <jaclkeen@gmail.com>
2016-08-11 15:26:23 -0500

    css styling changes

--- javascripts/battle.js
+++ javascripts/battle.js
@@ -24,33 +24,21 @@ Gauntlet.Combat = function(first, second) {
   var hp1 = first.health;
   var hp2 = second.health;
 
-  if (hp2 <= 0) {
-    $("#battleground").html("");
-    $("#battleground").prepend("<p>" + first.playerName + " won!")
-  } else if (hp1 <= 0) {
-    $("#battleground").html("");
-    $("#battleground").prepend("<p>" + second.playerName + " won!")
-  } else {
-    $("#battle_status_area").html(first.playerName + " attacked!")
-
-    second.health = Math.floor(hp2 - ((first.strength * .01 * first.weapon.damage) * .5))
-    // $('#player2_stats').html(`Your health is ${second.health}`)
-
-    first.health = Math.floor(hp2 - ((second.strength * .01 * second.weapon.damage) * .5))
-
-    // $("#player1_stats").html(`Your health is ${first.health}`);
-    // $("#player2_stats").html(`Enemy's health is ${second.health}`);
-  }
-
     $("#healthMsg").hide();
     $("#enemyHealthMsg").hide();
     $("#battle_status_area").text(first.playerName + " attacked!").hide().fadeIn(1000).fadeOut(1000, function() {
-      second.health = Math.floor(hp2 - ((first.strength * .01 * first.weapon.damage) * .5))
+      second.health = Math.floor(hp2 - ((first.strength * .01 * first.weapon.damage * 4) * .5))
       $("#battle_status_area").text(second.playerName + "'s health is now " + second.health).hide().fadeIn(1000).fadeOut(1000, function() {
       $("#player2_stats").html(second.playerName + "'s health is now " + second.health)
-        if (second.health <= 0) {
-
+        if (second.health < 0) {
+          second.health = 0
+          $("#player2_stats").html(`Enemy's health is ${second.health}`);
           $("#battle_status_area").show().text(first.playerName + " won!")
+        }
+        else if(first.health < 0){
+          first.health = 0
+          $("#player1_stats").html(`Your health is ${first.health}`);
+          $("#battle_status_area").show().text(second.playerName + " won!")
         }
         else {
           $("#battle_status_area").text("Now " + second.playerName + " attacked!").hide().fadeIn(1000).fadeOut(1000, function() {
@@ -58,7 +46,6 @@ Gauntlet.Combat = function(first, second) {
           $("#battle_status_area").text(first.playerName + "'s health is now " + first.health).hide().fadeIn(1000).fadeOut(1000, function() {
           $('#player1_stats').html(`Your health is ${first.health}`)
             if (first.health <= 0) {
-              $("#battle_status_area").show().text(second.playerName + " won!")
             }
         })
         });
